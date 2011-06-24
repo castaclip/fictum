3a96d5670aed865fd927f6719e6660c8b19dd9d8
Florian Gilcher <florian.gilcher@asquera.de>
2011-06-24 14:39:53 +0200

    Match on Regexp routes as well

--- frameworks/foundation/debug/url_types/regular_expression_url.js
+++ frameworks/foundation/debug/url_types/regular_expression_url.js
@@ -1,7 +1,11 @@
 sc_require('debug/url_types/base');
 
 Fictum.RegularExpressionUrl = Fictum.Url.extend({
-  matches: function(url) {
-    return url.match(this.get('url')) !== null;
+  matches: function(url, method) {
+    if (this.get('method')) {
+      return (url.match(this.get('url')) !== null && this.get('method') == method)
+    } else {
+      return url.match(this.get('url')) !== null;
+    }
   }
 });
