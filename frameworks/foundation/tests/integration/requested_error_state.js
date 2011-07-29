fd3fcdf76cf349452c8de55baedcb36204dea450
Sebastian Schulze <github.com@bascht.com>
2011-07-29 11:37:14 +0200

    - cleared up one namespace layer to fix beforeEach() behaviour

--- frameworks/foundation/tests/integration/requested_error_state.js
+++ frameworks/foundation/tests/integration/requested_error_state.js
@@ -8,56 +8,48 @@ describe('Scenario: Requesting with a non 200 status code', function() {
       Fictum.teardown();
     });
 
-    describe('And a URL that I want to stub', function() {
+    describe('When I register that URL with a non 200 status code', function() {
       var url;
       beforeEach(function() {
-        url = '/broken';
+        Fictum.registerUrl('/broken', "You made response kitty cry!", { status: 500 } );
+        console.log("ME REGISTY!");
       });
 
-      describe('When I register that URL with a non 200 status code', function() {
-      beforeEach(function() {
-          Fictum.registerUrl(url, "You made response kitty cry!", { status: 500 } );
-      });
-
-      describe('And I make an asynchronous request to that URL', function() {
-          var request, response;
-          beforeEach(function() {
-            request = SC.Request.getUrl('/broken')
-            request.set('isAsynchronous', true);
-            response = request.send();
-            waitsFor(function() {
-                return response.get('status') !== -100;
-            });
+      describe('And request the resource asynchronously', function() {
+        var request, response;
+        beforeEach(function() {
+          request = SC.Request.getUrl('/broken');
+          request.set('isAsynchronous', true);
+          response = request.send();
+          waitsFor(function() {
+            return response.get('status') !== -100;
           });
+        });
 
-          it('Then the response should be in an error state', function() {
-
+        it('Then the response should be in an error state', function() {
           expect(response.get('body')).toEqual("You made response kitty cry!");
           expect(response.get('status')).toEqual(500);
           expect(response.get('isError')).toEqual(YES);
           expect(SC.ok(response)).toEqual(false);
-          });
         });
       });
 
-      describe('And I make an asynchronous request to that URL', function() {
-          var request, response;
-          beforeEach(function() {
-            request = SC.Request.getUrl('/broken')
-            request.set('isAsynchronous', false);
-            response = request.send();
-            waitsFor(function() {
-                return response.get('status') !== -100;
-            });
+      describe('And request the resource synchronously', function() {
+        var request, response;
+        beforeEach(function() {
+          request = SC.Request.getUrl('/broken');
+          request.set('isAsynchronous', false);
+          response = request.send();
+          waitsFor(function() {
+            return response.get('status') !== -100;
           });
+        });
 
-          it('Then the response should be in an error state', function() {
-
+        it('Then the response should be in an error state', function() {
           expect(response.get('body')).toEqual("You made response kitty cry!");
           expect(response.get('status')).toEqual(500);
           expect(response.get('isError')).toEqual(YES);
           expect(SC.ok(response)).toEqual(false);
-          });
         });
       });
     });
